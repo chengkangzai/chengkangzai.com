32ffd4daf95cf3d92d674a7da5826f08a0bd7653
chengkangzai <pycck@hotmail.com>
2021-05-11 19:16:47 +0800

    add controller for comment and post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create(): Response
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post)
+    {
+        echo $post;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Post $post
+     * @return Response
+     */
+    public function edit(Post $post)
+    {
+        echo $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Post $post
+     * @return Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Post $post
+     * @return Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+    }
+}
