77e5905078663ea55a0b4dd91a048929a9081ffa
chengkangzai <pycck@hotmail.com>
2021-05-11 23:58:19 +0800

    use tailwind for authentication

--- resources/views/auth/forget-password.blade.php
+++ resources/views/auth/forget-password.blade.php
@@ -1,47 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="rounded-lg sm:border-2 px-4 my-40 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center bg-white dark:bg-gray-700 mx-auto">
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <form method="POST" action="{{ route('password.email') }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Send Password Reset Link') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+        <form class="text-center" method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <h1 class="tracking-wider mb-8 w-full text-gray-600 text-3xl dark:text-white">
+                {{__('Reset Password')}}
+            </h1>
+            @if ($errors->any())
+                <div
+                    class="block text-sm text-left text-red-600 bg-red-200 border border-red-400 h-auto flex items-center p-4 rounded-sm"
+                    role="alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="flex items-start">
+                                <p class="ml-2">
+                                    {{$error}}
+                                </p>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
+            @endif
+            <div class="py-2 text-left">
+                <input type="email" name="email"
+                       class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700"
+                       placeholder="{{ __('E-Mail Address') }}"/>
             </div>
+
+            <div class="py-2">
+                <button type="submit"
+                        class="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg from-blue-400 bg-gradient-to-r to-purple-600 dark:bg-white">
+                    {{ __('Send Password Reset Link') }}
+                </button>
+            </div>
+
+        </form>
+
+        <div class="text-center ">
+            <a class="hover:underline dark:text-white" href="{{ route('login') }}">
+                {{ __('Login Here') }}
+            </a>
         </div>
     </div>
 @endsection
