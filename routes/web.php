a0f675a22482aa99ce8fb2b6dc6336a8b12453da
chengkangzai <pycck@hotmail.com>
2021-05-11 19:13:53 +0800

    added router for Post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('{post:slug}', [PostController::class, 'show']);
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('create', [PostController::class, 'create']);
+        Route::get('{post}/edit', [PostController::class, 'edit']);
+        Route::put('{post}', [PostController::class, 'update']);
+        Route::delete('{post}', [PostController::class, 'destroy']);
+    });
+
 });
 
 Route::view('home', 'home');
