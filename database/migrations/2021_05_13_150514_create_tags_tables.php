e0068a7d5f82a54a5fb5a2772af8ba0d5d473696
chengkangzai <pycck@hotmail.com>
2021-05-16 19:25:19 +0800

    use back json field

--- database/migrations/2021_05_13_150514_create_tags_tables.php
+++ database/migrations/2021_05_13_150514_create_tags_tables.php
@@ -11,8 +11,8 @@ class CreateTagsTables extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
 
-            $table->longText('name');
-            $table->longText('slug');
+            $table->json('name');
+            $table->json('slug');
             $table->string('type')->nullable();
             $table->integer('order_column')->nullable();
 
