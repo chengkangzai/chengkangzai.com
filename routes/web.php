10c90ef4e8fd2b3f6964159a9ab68d1801da164c
chengkangzai <pycck@hotmail.com>
2021-05-17 22:41:03 +0800

    remove subdomain bindind

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PublicPostCommentController;
 use App\Http\Controllers\PublicPostController;
+use App\Http\Controllers\WorksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,27 +19,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'public.'], function () {
 
-//    Route::get('/', function () {
-//
-//    });
     Route::view('/', 'welcome')->name('index');
     Route::view('about', 'public.about')->name('about');
     Route::view('work', 'public.work')->name('work');
 });
 
-Route::domain('blog.' . env('APP_DOMAIN'))->group(function () {
-    Route::group(['prefix' => 'admin', 'middleware' => 'auth', 'as' => 'admin.'], function () {
-        Route::view('home', 'home')->name('home');
-        Route::resource('posts', PostController::class);
-    });
 
-    Route::group(['as' => 'public.'], function () {
-        Route::group(['prefix' => 'posts', 'as' => 'posts.'], function () {
-            Route::get('/', [PublicPostController::class, 'index'])->name('index');
-            Route::get('{post:slug}', [PublicPostController::class, 'show'])->name('show');
-        });
-        Route::resource('posts.comments', PublicPostCommentController::class)->only(['store']);
-    });
-
+Route::group(['prefix' => 'admin', 'middleware' => 'auth', 'as' => 'admin.'], function () {
+    Route::view('home', 'home')->name('home');
+    Route::resource('posts', PostController::class);
+    Route::resource('works', WorksController::class);
 });
+
+Route::group(['as' => 'public.'], function () {
+    Route::group(['prefix' => 'posts', 'as' => 'posts.'], function () {
+        Route::get('/', [PublicPostController::class, 'index'])->name('index');
+        Route::get('{post:slug}', [PublicPostController::class, 'show'])->name('show');
+    });
+    Route::resource('posts.comments', PublicPostCommentController::class)->only(['store']);
+});
+
+
 
