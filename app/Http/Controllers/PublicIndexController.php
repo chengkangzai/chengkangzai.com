029b78be141e0a85ba399295e8166bfe56d4682c
chengkangzai <pycck@hotmail.com>
2021-05-25 16:33:47 +0800

    take 3 instead of 6

--- app/Http/Controllers/PublicIndexController.php
+++ app/Http/Controllers/PublicIndexController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Works;
-use Illuminate\Http\Request;
+use Exception;
 use Storage;
 
 class PublicIndexController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function index()
     {
         $works = cache()->remember('public-Works', 60 * 60 * 24, function () {
-            return Works::active()->take(3)->get()->filter(function ($work) {
+            return Works::active()->take(6)->get()->filter(function ($work) {
                 $s3 = Storage::disk('s3');
                 $client = $s3->getDriver()->getAdapter();
                 $imgLink = $s3->getAwsTemporaryUrl($client, Works::S3_PATH . '/' . $work->picture_name, now()->addHours(24), []);
