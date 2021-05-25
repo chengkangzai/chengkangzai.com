fd0367cacdbef90bdeb71c83e9ad5739729a04f5
chengkangzai <pycck@hotmail.com>
2021-05-25 17:27:27 +0800

    dont seed CommentSeeder.php

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -14,6 +14,6 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        Comment::factory()->count(10)->create();
+//        Comment::factory()->count(10)->create();
     }
 }
