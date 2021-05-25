2f7da814b7224156e5050517299801c3b168823b
chengkangzai <pycck@hotmail.com>
2021-05-25 16:02:29 +0800

    use real work

--- database/seeders/WorksSeeder.php
+++ database/seeders/WorksSeeder.php
@@ -14,6 +14,15 @@ class WorksSeeder extends Seeder
      */
     public function run()
     {
-        Works::factory()->count(10)->create();
+//        Works::factory()->count(10)->create();
+        $work = Works::Create([
+            'name' => "What to Ear Huh",
+            'description' => "Are you having trouble of what to eat day ? What to app will be the essential app for you to decide what to eat no matter when!",
+            'picture_name' => "UwLb7RZ1PRdp9biAIppHK41k4RCQkV3XqY7ORQgO.png",
+            'url' => "https://eat.chengkangzai.com",
+            'github_url' => "https://github.com/chengkangzai/eatwhat",
+            'status' => 1,
+        ]);
+        $work->syncTags(['Firebase', 'Laravel', 'Angular', 'Ionic', 'Capacitor']);
     }
 }
