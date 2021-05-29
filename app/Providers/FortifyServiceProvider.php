1b96b18add267812fb2fbcb018278efc9e4b5e19
chengkangzai <pycck@hotmail.com>
2021-05-29 22:57:04 +0800

    Add SEO for login and request password link

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,6 +6,7 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
@@ -44,12 +45,14 @@ class FortifyServiceProvider extends ServiceProvider
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
         Fortify::loginView(function () {
+            SEOTools::setTitle("Login");
             return view('auth.login');
         });
 //        Fortify::registerView(function () {
 //            return view('auth.register');
 //        });
         Fortify::requestPasswordResetLinkView(function () {
+            SEOTools::setTitle('Forget Password');
             return view('auth.forget-password');
         });
         Fortify::resetPasswordView(function () {
