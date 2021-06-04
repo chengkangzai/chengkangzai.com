a941d5cbcccfd29c06659e7843edd0817a395c8a
chengkangzai <pycck@hotmail.com>
2021-06-05 01:08:36 +0800

    change the font contrast

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @push('cdn')
-
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 @endpush
 @push('style')
@@ -57,7 +56,7 @@
                         class="text-2xl text-base leading-relaxed text-left text-gray-400 inline-block dark:text-gray-200"
                         id="typed"></span>
                     </div>
-                    <p class="w-full mt-2 mb-8 text-sm text-left text-gray-600">
+                    <p class="w-full mt-2 mb-8 text-sm text-left dark:text-gray-400 text-dark-600">
                         ⚡️ Fun fact: I convert Coffee to Code ☕️
                     </p>
                     <div class="flex flex-col justify-center lg:flex-row">
@@ -170,7 +169,8 @@
                                     <path xmlns="http://www.w3.org/2000/svg"
                                           d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"/>
                                 </svg>
-                                <a class="text-lg underline" rel="noreferrer" target="_blank" aria-label="My Github Profile"
+                                <a class="text-lg underline" rel="noreferrer" target="_blank"
+                                   aria-label="My Github Profile"
                                    href="https://www.github.com/chengkangzai">@chengkangzai</a>
                             </p>
                             <p class="text-white text-gray-600 dark:text-gray-200">
@@ -179,7 +179,8 @@
                                     <path
                                         d="M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.61C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.79,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM170.87,405.43H106.69V205.88h64.18ZM141,175.54h-.46c-20.54,0-33.84-15.29-33.84-34.43,0-19.49,13.65-34.42,34.65-34.42s33.85,14.82,34.31,34.42C175.65,160.25,162.35,175.54,141,175.54ZM405.43,405.43H341.25V296.32c0-26.14-9.34-44-32.56-44-17.74,0-28.24,12-32.91,23.69-1.75,4.2-2.22,9.92-2.22,15.76V405.43H209.38V205.88h64.18v27.77c9.34-13.3,23.93-32.44,57.88-32.44,42.13,0,74,27.77,74,87.64Z"/>
                                 </svg>
-                                <a class="text-lg underline" rel="noreferrer" target="_blank" aria-label="My Linkedin Profile"
+                                <a class="text-lg underline" rel="noreferrer" target="_blank"
+                                   aria-label="My Linkedin Profile"
                                    href="https://www.linkedin.com/in/chingchengkang/">Ching Cheng Kang</a>
                             </p>
                             <p class="text-white text-gray-600 dark:text-gray-200">
@@ -188,7 +189,8 @@
                                     <path
                                         d="M496,109.5a201.8,201.8,0,0,1-56.55,15.3,97.51,97.51,0,0,0,43.33-53.6,197.74,197.74,0,0,1-62.56,23.5A99.14,99.14,0,0,0,348.31,64c-54.42,0-98.46,43.4-98.46,96.9a93.21,93.21,0,0,0,2.54,22.1,280.7,280.7,0,0,1-203-101.3A95.69,95.69,0,0,0,36,130.4C36,164,53.53,193.7,80,211.1A97.5,97.5,0,0,1,35.22,199v1.2c0,47,34,86.1,79,95a100.76,100.76,0,0,1-25.94,3.4,94.38,94.38,0,0,1-18.51-1.8c12.51,38.5,48.92,66.5,92.05,67.3A199.59,199.59,0,0,1,39.5,405.6,203,203,0,0,1,16,404.2,278.68,278.68,0,0,0,166.74,448c181.36,0,280.44-147.7,280.44-275.8,0-4.2-.11-8.4-.31-12.5A198.48,198.48,0,0,0,496,109.5Z"/>
                                 </svg>
-                                <a class="text-lg underline" rel="noreferrer" target="_blank" aria-label="My Twitter Account"
+                                <a class="text-lg underline" rel="noreferrer" target="_blank"
+                                   aria-label="My Twitter Account"
                                    href="https://twitter.com/chengkangzai">@chengkangzai</a>
                             </p>
                             <p class="text-white text-gray-600 dark:text-gray-200">
@@ -221,7 +223,8 @@
                         class="w-full mx-auto rounded-lg bg-gray-50 dark:bg-gray-800 shadow-lg text-gray-800 px-5 transform hover:scale-110 transition duration-300">
                         <div class="w-full mb-10 mt-5">
                             <div class="w-full">
-                                <a href="{{$work->url ?? "#"}}" target="_blank" rel="noreferrer" aria-label="Link to open {{$work->name}}">
+                                <a href="{{$work->url ?? "#"}}" target="_blank" rel="noreferrer"
+                                   aria-label="Link to open {{$work->name}}">
                                     <img src="{{$work->imgLink ?? "#"}}" alt=""
                                          class="w-full h-72 rounded-lg mb-3 object-cover"
                                          loading="lazy">
@@ -232,7 +235,8 @@
                             <div
                                 class="text-center mt-2 leading-none flex items-start w-full mt-4 text-blue-700 dark:text-blue-400 mb-4 space-x-2">
                                 @if($work->url)
-                                    <a rel="noopener" target="_blank" href="{{$work->url ?? "#"}}" aria-label="Link to open {{$work->name}}">
+                                    <a rel="noopener" target="_blank" href="{{$work->url ?? "#"}}"
+                                       aria-label="Link to open {{$work->name}}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
                                              viewBox="0 0 24 24"
                                              class="dark:text-blue-400 fill-current">
@@ -242,7 +246,8 @@
                                     </a>
                                 @endif
                                 @if($work->github_url)
-                                    <a href="{{$work->github_url}}" rel="noopener" target="_blank" aria-label="Link to open Project Repository">
+                                    <a href="{{$work->github_url}}" rel="noopener" target="_blank"
+                                       aria-label="Link to open Project Repository">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
                                              viewBox="0 0 24 24"
                                              class="dark:text-white fill-current">
