4a1aba7666228a09159d20fb33922ff7922032a9
chengkangzai <pycck@hotmail.com>
2021-06-21 17:35:15 +0800

    change h1 to h2 for unique heading for each view

--- resources/views/partial/public-heading.blade.php
+++ resources/views/partial/public-heading.blade.php
@@ -1,4 +1,4 @@
 <div class="dark:text-white mb-12 pb-12 border-b-2">
-    <h1 class="text-4xl inline pr-5">{{config('app.name')}}</h1> ||
+    <h2 class="text-4xl inline pr-5">{{config('app.name')}}</h2> ||
     <p class="inline pl-5">{{__('Just Random Sharing Blog for tech')}}</p>
 </div>
