9a70298b7caf99cbc3ee7e40a5fdd58d937a87d3
chengkangzai <pycck@hotmail.com>
2021-07-30 00:57:13 +0800

    add SEO Description

--- app/Http/Controllers/PublicPandemicController.php
+++ app/Http/Controllers/PublicPandemicController.php
@@ -10,6 +10,7 @@ class PublicPandemicController extends Controller
     public function index()
     {
         SEO::setTitle(__('COVID Dashboard'));
+        SEO::setDescription(__('A Dashboard to monitoring Covid 19 Cases, HealthCare Facilities and Vaccine Status'));
         $dashboardValue = app(CovidService::class)->getDashboardValue();
 
         return view('public.covid.index', compact('dashboardValue'));
