744181961662f40a6a7a183edf72532d47ab87bf
chengkangzai <pycck@hotmail.com>
2021-09-05 14:12:35 +0800

    convert Covid Malaysia component to use own services instead of using covid Service

--- app/Http/Livewire/CovidDashboardMalaysia.php
+++ app/Http/Livewire/CovidDashboardMalaysia.php
@@ -3,11 +3,39 @@
 namespace App\Http\Livewire;
 
 
-class CovidDashboardMalaysia extends CovidComponent
+use App\Http\Services\CasesMalaysiaService;
+use App\Models\Covid\CasesMalaysia;
+use App\Models\Covid\DeathsMalaysia;
+use App\Models\Covid\TestMalaysia;
+use App\Models\Covid\VaxMalaysia;
+use App\Models\Covid\VaxRegMalaysia;
+use Livewire\Component;
+
+class CovidDashboardMalaysia extends Component
 {
 
-    public function render()
+    public CasesMalaysia $cases;
+    public DeathsMalaysia $death;
+    public TestMalaysia $test;
+    public VaxMalaysia $vax;
+    public VaxRegMalaysia $vaxReg;
+
+    public int $clusterCount;
+    public int|float $positiveRate;
+    public int|float $fatalityRate;
+
+    public function render(CasesMalaysiaService $service)
     {
+        $this->cases = $service->getCases();
+        $this->death = $service->getDeath();
+        $this->test = $service->getTest();
+        $this->vax = $service->getVax();
+        $this->vaxReg = $service->getVaxReg();
+
+        $this->clusterCount = $service->getClusterCount();
+        $this->positiveRate = $service->calcPositiveRate();
+        $this->fatalityRate = $service->calcFatalityRate();
+
         return view('livewire.covid-dashboard-malaysia');
     }
 }
