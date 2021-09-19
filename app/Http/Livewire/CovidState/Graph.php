15f3180be49dce0b3be2966b4394034744987b7e
chengkangzai <pycck@hotmail.com>
2021-09-19 14:35:08 +0800

    add graph

--- app/Http/Livewire/CovidState/Graph.php
+++ app/Http/Livewire/CovidState/Graph.php
@@ -2,12 +2,93 @@
 
 namespace App\Http\Livewire\CovidState;
 
+use App\Http\Services\CovidState\Graph\CovidStateGraphService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Graph extends Component
 {
-    public function render()
+    public string $state = 'Johor';
+    public string $filter = 'TWO_WEEK';
+
+    protected $listeners = ['CovidStateUpdate'];
+
+    public Collection $date;
+    public Collection $confirmCase;
+    public Collection $recoveredCase;
+    public Collection $deathCase;
+    public Collection $activeCase;
+    public Collection $bidCase;
+
+    public Collection $cat1;
+    public Collection $cat2;
+    public Collection $cat3;
+    public Collection $cat4;
+    public Collection $cat5;
+
+    public function render(CovidStateGraphService $service): Factory|View|Application
     {
+        $cases = $service->getCases($this->state, $this->filter);
+        $deaths = $service->getDeath($this->state, $this->filter);
+        $healthCareCategory = $service->getHealthCare($this->state, $this->filter);
+
+
+        $this->initCasesVariable($cases, $deaths);
+        $this->initHealthCareVariable($healthCareCategory);
+
+        $this->notifyChild();
+
         return view('livewire.covid-state.graph');
     }
+
+    public function updatedState()
+    {
+        $this->emit("CovidStateUpdate", $this->state);
+    }
+
+    public function CovidStateUpdate(string $state)
+    {
+        $this->state = $state;
+    }
+
+    public function notifyChild()
+    {
+        $this->dispatchBrowserEvent('CovidStateUpdate', [
+            'date' => $this->date,
+            'confirmCase' => $this->confirmCase,
+            'recoveredCase' => $this->recoveredCase,
+            'deathCase' => $this->deathCase,
+            'bidCase' => $this->bidCase,
+            'activeCase' => $this->activeCase,
+            'cat1' => $this->cat1,
+            'cat2' => $this->cat2,
+            'cat3' => $this->cat3,
+            'cat4' => $this->cat4,
+            'cat5' => $this->cat5,
+        ]);
+    }
+
+
+    public function initHealthCareVariable(Collection $healthCareCategory): void
+    {
+        $this->cat1 = $healthCareCategory->pluck('cat1', 'date');
+        $this->cat2 = $healthCareCategory->pluck('cat2', 'date');
+        $this->cat3 = $healthCareCategory->pluck('cat3', 'date');
+        $this->cat4 = $healthCareCategory->pluck('cat4', 'date');
+        $this->cat5 = $healthCareCategory->pluck('cat5', 'date');
+    }
+
+    public function initCasesVariable(Collection $cases, Collection $deaths): void
+    {
+        $this->date = $cases->pluck('date')->map(fn($date) => $date->toDateString());
+        $this->confirmCase = $cases->pluck('cases_new');
+        $this->recoveredCase = $cases->pluck('cases_recovered');
+        $this->deathCase = $deaths->pluck('deaths_new');
+        $this->bidCase = $deaths->pluck('deaths_bid');
+        $this->activeCase = $cases->pluck('activeCase');
+    }
+
 }
