3e66bebee93c729e2a84eed36cbbf14dde3b7124
chengkangzai <pycck@hotmail.com>
2021-09-19 16:38:15 +0800

    reformat code and improve Mobile UI

--- resources/views/livewire/covid-state/head.blade.php
+++ resources/views/livewire/covid-state/head.blade.php
@@ -1,10 +1,12 @@
-<section class="mt-2 rounded-2xl dark:bg-white dark:text-black py-8 bg-gray-50 shadow" id="malaysia-dashboard" wire:loading.class="animate-pulse"
+<section class="mt-2 rounded-2xl dark:bg-white dark:text-black py-8 bg-gray-50 shadow" id="malaysia-dashboard"
+         wire:loading.class="animate-pulse"
          xmlns:wire="http://www.w3.org/1999/xhtml">
     <a href="#malaysia-dashboard">
         <h1 class="text-2xl sm:text-5xl font-bold px-2">{{__('Covid-19 Dashboard')}} : {{__($state)}}</h1>
     </a>
-    <div class="container flex flex-row-reverse">
+    <div class="container space-y-2 sm:space-y-0 sm:flex sm:flex-row-reverse">
         <label>
+            {{__('Filter By')}} :
             <select class="bg-white ring ring-gray-200 mx-2 px-4 py-1 rounded" wire:model="state">
                 <option disabled>-----</option>
                 @foreach(\App\Models\Covid\CasesState::STATE as $key => $filter)
@@ -14,6 +16,5 @@
                 @endforeach
             </select>
         </label>
-        <span class="py-1">{{__('Filter By')}} : </span>
     </div>
 </section>
