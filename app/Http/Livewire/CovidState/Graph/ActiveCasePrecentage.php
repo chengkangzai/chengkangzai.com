d3ebb2dd46f4d072fde25de36987c309de430c0b
chengkangzai <pycck@hotmail.com>
2021-09-19 14:37:59 +0800

    add active Case percentage

---
+++ app/Http/Livewire/CovidState/Graph/ActiveCasePrecentage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\CovidState\Graph;
+
+use Livewire\Component;
+
+class ActiveCasePrecentage extends Component
+{
+    public function render()
+    {
+        return view('livewire.covid-state.graph.active-case-precentage');
+    }
+}
