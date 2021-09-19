e4062b59bb3809eaf51ed30d1bbfe3317e42f82a
chengkangzai <pycck@hotmail.com>
2021-09-19 14:35:53 +0800

    add active case

---
+++ app/Http/Livewire/CovidState/Graph/ActiveCaseVsHealthCare.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Livewire\CovidState\Graph;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+class ActiveCaseVsHealthCare extends Component
+{
+    public Collection $date;
+    public Collection $activeCase;
+
+    public Collection $cat1;
+    public Collection $cat2;
+    public Collection $cat3;
+    public Collection $cat4;
+    public Collection $cat5;
+
+
+    public function render(): Factory|View|Application
+    {
+        return view('livewire.covid-state.graph.active-case-vs-health-care');
+    }
+}
