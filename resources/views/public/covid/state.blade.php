943d28342a9bf2da891149c61bba4ddbcea2d15f
chengkangzai <pycck@hotmail.com>
2021-09-29 23:14:09 +0800

    add to the top rocket

--- resources/views/public/covid/state.blade.php
+++ resources/views/public/covid/state.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.covid_app')
 
 @section('content')
+    @include('partial.rocket')
     <div class="mx-2">
         <livewire:covid-state.head/>
         <livewire:covid-state.dashboard/>
