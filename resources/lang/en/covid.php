84f03e8a049e16dd1384ce0a5bf4f9719de9815b
chengkangzai <pycck@hotmail.com>
2021-10-05 23:19:28 +0800

    add custom language dile

---
+++ resources/lang/en/covid.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+  'New Case' => 'New Case :Day',
+  'Deaths' => 'Deaths :Day',
+  'New recovered Case' => 'New recovered Case :Day',
+  'New Dose 1 Jabbed' => 'New Dose 1 Jabbed :Day',
+  'New Dose 2 Jabbed' => 'New Dose 2 Jabbed :Day',
+  'Tested' => 'Tested :Day',
+  'Cumulative Case' => 'As of :Day, Cumulative Case',
+  'Active Case' => 'As of :Day, Active Case',
+  'Cumulative Death' => 'As of :Day, Cumulative Death',
+  'Cumulative recovered' => 'As of :Day, Cumulative recovered',
+  'Active Cluster' => 'As of :Day, Active Cluster',
+  'Percentage of Vaccine Register' => 'As of :Day, Vaccine Registrant',
+  'Active Cluster In Malaysia' => 'As of :Day, Active Cluster In Malaysia',
+  'Active Cases In Malaysia' => 'As of :Day, Active Cases In Malaysia',
+);
