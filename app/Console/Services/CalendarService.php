db21e2cee56332f35784e627e977a97cc81cf387
chengkangzai <pycck@hotmail.com>
2021-11-27 03:14:15 +0800

    add services

--- app/Console/Services/CalendarService.php
+++ app/Console/Services/CalendarService.php
@@ -4,20 +4,80 @@
 namespace App\Console\Services;
 
 
+use App\Http\Services\APUScheduleService;
+use App\Http\Services\MicrosoftGraphService;
+use App\Http\Services\TimeZoneService;
 use App\Models\User;
+use JetBrains\PhpStorm\ArrayShape;
+use Microsoft\Graph\Model\Event;
 
 class CalendarService
 {
-    public function addEvent($schedule, User $user)
+    private MicrosoftGraphService $graphService;
+    private APUScheduleService $APUSchedule;
+
+    public function __construct(MicrosoftGraphService $graphService, APUScheduleService $APUScheduleService)
     {
-        $this->format($schedule, $user);
+        $this->graphService = $graphService;
+        $this->APUSchedule = $APUScheduleService;
     }
 
-    private function format($schedule, User $user)
+    public function addEvent($config, User $user)
     {
-        $collection = collect();
-        foreach ($schedule as $key => $value) {
-        //TODO
+        $attendeeAddresses = explode(';', $user->email);
+        $attendees = $this->getAttendees($attendeeAddresses);
+        $schedules = $this->APUSchedule->getSchedule($config->intake_code, $config->grouping)->get();
+
+        foreach ($schedules as $schedule) {
+            $newEvent = $this->getNewEvent($schedule, $attendees, $user);
+
+            $this->syncCalendar($newEvent);
         }
+    }
+
+    private function getAttendees(array $attendeeAddresses): array
+    {
+        $attendees = [];
+        foreach ($attendeeAddresses as $attendeeAddress) {
+            array_push($attendees, [
+                'emailAddress' => [
+                    'address' => $attendeeAddress
+                ],
+                'type' => 'required'
+            ]);
+        }
+        return $attendees;
+    }
+
+    private function syncCalendar(array $newEvent)
+    {
+        $graph = $this->graphService->getGraph();
+        $graph->createRequest('POST', '/me/events')
+            ->attachBody($newEvent)
+            ->setReturnType(Event::class)
+            ->execute();
+    }
+
+    #[ArrayShape(['subject' => "", 'attendees' => "array", 'start' => "array", 'end' => "array", 'body' => "string[]"])]
+    private function getNewEvent($schedule, array $attendees, User $user): array
+    {
+        return [
+            'subject' => $schedule->MODID,
+            'attendees' => $attendees,
+            'start' => [
+                'dateTime' => $schedule->TIME_FROM_ISO,
+                'timeZone' => TimeZoneService::$timeZoneMap['SE Asia Standard Time']
+            ],
+            'end' => [
+                'dateTime' => $schedule->TIME_TO_ISO,
+                'timeZone' => TimeZoneService::$timeZoneMap['SE Asia Standard Time']
+            ],
+            'body' => [
+                'content' =>
+                    "Hi, $user->name, you have a class of $schedule->MODULE_NAME with lecturer $schedule->NAME ()" .
+                    " at $schedule->ROOM from $schedule->TIME_FROM to $schedule->TIME_TO",
+                'contentType' => 'text'
+            ]
+        ];
     }
 }
---
+++ app/Http/Services/MicrosoftGraphService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Services;
+
+use League\OAuth2\Client\Provider\GenericProvider;
+use Microsoft\Graph\Graph as MicrosoftGraph;
+
+class MicrosoftGraphService
+{
+    private TokenService $tokenService;
+
+    public function __construct()
+    {
+        $this->tokenService = new TokenService();
+    }
+
+    public function getOAuthClient(): GenericProvider
+    {
+        return new GenericProvider([
+            'clientId' => config('azure.appId'),
+            'clientSecret' => config('azure.appSecret'),
+            'redirectUri' => config('azure.redirectUri'),
+            'urlAuthorize' => config('azure.authority') . config('azure.authorizeEndpoint'),
+            'urlAccessToken' => config('azure.authority') . config('azure.tokenEndpoint'),
+            'urlResourceOwnerDetails' => '',
+            'scopes' => config('azure.scopes')
+        ]);
+    }
+
+    public function getGraph(): MicrosoftGraph
+    {
+        $accessToken = $this->tokenService->getAccessToken(auth()->user());
+
+        // Create a Graph client
+        $graph = new MicrosoftGraph();
+        $graph->setAccessToken($accessToken);
+        return $graph;
+    }
+}
