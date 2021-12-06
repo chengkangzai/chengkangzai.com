2c89bb3143975a823d101be468fc7c63ecdd8879
chengkangzai <pycck@hotmail.com>
2021-12-06 22:09:46 +0800

    update the name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
                             </svg>
-                            {{__('Setting')}}
+                            {{__('Schedule')}}
                         </a>
                     @endcanany
                 @endif
