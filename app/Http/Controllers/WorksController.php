366a6aa4e0eced704a221010d008126bb279f930
chengkangzai <pycck@hotmail.com>
2021-12-12 23:00:20 +0800

    fix word is not translated

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -45,7 +45,7 @@ class WorksController extends Controller
                 }
                 return $work;
             });
-            return redirect()->route('admin.works.index')->with('success', 'Work updated successfully');
+            return redirect()->route('admin.works.index')->with('success', __('Work updated successfully'));
         } catch (Throwable|Exception $e) {
             Storage::disk('s3')->delete($path);
             return redirect()->route('admin.works.index')->withErrors(__('Whoops! Something went wrong. Please try again later.'));
@@ -91,7 +91,7 @@ class WorksController extends Controller
                 }
                 return $work;
             });
-            return redirect()->route('admin.works.index')->with('success', 'Work updated successfully');
+            return redirect()->route('admin.works.index')->with('success', __('Work updated successfully'));
         } catch (Throwable $e) {
             return redirect()->route('admin.works.index')->withErrors(__('Whoops! Something went wrong. Please try again later.'));
         }
