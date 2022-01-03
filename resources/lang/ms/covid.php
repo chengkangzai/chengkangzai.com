37f2a8f3dd77fc432ae77f3e3063158046903f8f
chengkangzai <pycck@hotmail.com>
2022-01-03 23:58:41 +0800

    fix array key should not contain space

--- resources/lang/ms/covid.php
+++ resources/lang/ms/covid.php
@@ -1,18 +1,18 @@
 <?php
 
-return array (
-  'New Case' => 'Kes Baru :Day',
-  'Deaths' => 'Kematian :Day',
-  'New recovered Case' => 'Kes :Day yang baru dipulihkan',
-  'New Dose 1 Jabbed' => 'Dos Baru 1 Jabbed :Day',
-  'New Dose 2 Jabbed' => 'Dos Baru 2 Jabbed :Day',
-  'Tested' => 'Diuji :Day',
-  'Cumulative Case' => 'Pada :Day, Kes Kumulatif',
-  'Active Case' => 'Pada :Day, Kes Aktif',
-  'Cumulative Death' => 'Pada :Day, Kematian Kumulatif',
-  'Cumulative recovered' => 'Pada :Day, Kumulatif pulih',
-  'Active Cluster' => 'Pada :Day, Kluster Aktif',
-  'Percentage of Vaccine Register' => 'Pada :Day, Pendaftar Vaksin',
-  'Active Cluster In Malaysia' => 'Pada :Day, Kluster Aktif Di Malaysia',
-  'Active Cases In Malaysia' => 'Pada :Day, Kes Aktif Di Malaysia',
+return array(
+    'New_Case' => 'Kes Baru :Day',
+    'Deaths' => 'Kematian :Day',
+    'New_recovered_Case' => 'Kes :Day yang baru dipulihkan',
+    'New_Dose_1_Jabbed' => 'Dos Baru 1 Jabbed :Day',
+    'New_Dose_2_Jabbed' => 'Dos Baru 2 Jabbed :Day',
+    'Tested' => 'Diuji :Day',
+    'Cumulative_Case' => 'Pada :Day, Kes Kumulatif',
+    'Active_Case' => 'Pada :Day, Kes Aktif',
+    'Cumulative_Death' => 'Pada :Day, Kematian Kumulatif',
+    'Cumulative_recovered' => 'Pada :Day, Kumulatif pulih',
+    'Active_Cluster' => 'Pada :Day, Kluster Aktif',
+    'Percentage_of_Vaccine_Register' => 'Pada :Day, Pendaftar Vaksin',
+    'Active_Cluster_In_Malaysia' => 'Pada :Day, Kluster Aktif Di Malaysia',
+    'Active_Cases_In_Malaysia' => 'Pada :Day, Kes Aktif Di Malaysia',
 );
--- resources/lang/zh/covid.php
+++ resources/lang/zh/covid.php
@@ -1,18 +1,18 @@
 <?php
 
-return array (
-  'New Case' => ':Day新案例',
-  'Deaths' => ':Day死亡',
-  'New recovered Case' => ':Day新康复案例',
-  'New Dose 1 Jabbed' => ':Day新增第一剂接种',
-  'New Dose 2 Jabbed' => ':Day新增第二剂接种',
-  'Tested' => ':Day的测试人数',
-  'Cumulative Case' => '截止:Day的累积案例',
-  'Active Case' => '截止:Day的活跃案例',
-  'Cumulative Death' => '截止:Day的累计死亡',
-  'Cumulative recovered' => '截止:Day的累计恢复',
-  'Active Cluster' => '截止:Day的活动集群',
-  'Percentage of Vaccine Register' => '截止:Day的疫苗登记',
-  'Active Cluster In Malaysia' => '截止:Day的活跃感染群',
-  'Active Cases In Malaysia' => '截止:Day马来西亚的活跃病例',
+return array(
+    'New_Case' => ':Day新案例',
+    'Deaths' => ':Day死亡',
+    'New_recovered_Case' => ':Day新康复案例',
+    'New_Dose_1_Jabbed' => ':Day新增第一剂接种',
+    'New Dose_2_Jabbed' => ':Day新增第二剂接种',
+    'Tested' => ':Day的测试人数',
+    'Cumulative_Case' => '截止:Day的累积案例',
+    'Active_Case' => '截止:Day的活跃案例',
+    'Cumulative_Death' => '截止:Day的累计死亡',
+    'Cumulative_recovered' => '截止:Day的累计恢复',
+    'Active_Cluster' => '截止:Day的活动集群',
+    'Percentage_of_Vaccine_Register' => '截止:Day的疫苗登记',
+    'Active_Cluster_In_Malaysia' => '截止:Day的活跃感染群',
+    'Active_Cases_In_Malaysia' => '截止:Day马来西亚的活跃病例',
 );
