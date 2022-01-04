36cad09e4db22ed56eaae0ba60592da5eaafd852
chengkangzai <pycck@hotmail.com>
2022-01-05 00:40:06 +0800

    remove comment

--- app/Console/Commands/SyncScheduleToCalendarCommand.php
+++ app/Console/Commands/SyncScheduleToCalendarCommand.php
@@ -8,35 +8,15 @@ use Illuminate\Console\Command;
 
 class SyncScheduleToCalendarCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'calendar:sync';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'run sync command to sync apu schedule to calendar';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle(): int
     {
         $this->info('Starting sync schedule to calendar');
