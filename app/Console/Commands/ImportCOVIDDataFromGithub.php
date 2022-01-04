aea89c24c5695e5bc002a7fc29bb611eca0668e3
chengkangzai <pycck@hotmail.com>
2022-01-05 00:39:08 +0800

    refactor code to remove redundancy

--- app/Console/Commands/ImportCOVIDDataFromGithub.php
+++ app/Console/Commands/ImportCOVIDDataFromGithub.php
@@ -4,51 +4,39 @@ namespace App\Console\Commands;
 
 use App\Console\Services\ImportCovidFromGithubService;
 use App\Http\Services\WebHookService;
-use Artisan;
+use App\Models\Covid\CasesMalaysia;
+use App\Models\Covid\CasesState;
+use App\Models\Covid\Cluster;
+use App\Models\Covid\DeathsMalaysia;
+use App\Models\Covid\DeathsState;
+use App\Models\Covid\Hospital;
+use App\Models\Covid\ICU;
+use App\Models\Covid\PKRC;
+use App\Models\Covid\Population;
+use App\Models\Covid\TestMalaysia;
+use App\Models\Covid\TestState;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 use Exception;
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Throwable;
 
 class ImportCOVIDDataFromGithub extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'import:covid {--force}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Import Covid Cases From Github';
 
-    /**
-     * @var ImportCovidFromGithubService|Application|mixed
-     */
     private ImportCovidFromGithubService $covidService;
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct(ImportCovidFromGithubService $service)
     {
         parent::__construct();
         $this->covidService = $service;
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
         try {
@@ -68,7 +56,7 @@ class ImportCOVIDDataFromGithub extends Command
             $this->importICU();
             $this->importPKRC();
 
-            Artisan::call('cache:clear');
+            Cache::clear();
             if (app()->environment('production')) {
                 app(WebHookService::class)->notifyInSpam(Carbon::now() . ' : Covid Data Successfully Inserted', WebHookService::COLOR['GREEN']);
             }
@@ -94,269 +82,90 @@ class ImportCOVIDDataFromGithub extends Command
         DB::table('populations')->truncate();
     }
 
-
     public function importCasesMalaysia()
     {
         $records = $this->covidService->getCasesMalaysia();
-
-        if (DB::table('cases_malaysia')->count() == $records->count()) {
-            $this->info('[CaseMalaysia] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('cases_malaysia')->truncate();
-
-        $this->info('[CaseMalaysia] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('cases_malaysia')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'cases_malaysia', CasesMalaysia::class);
     }
 
     public function importCasesState()
     {
         $records = $this->covidService->getCasesState();
-
-        if (DB::table('cases_states')->count() == $records->count()) {
-            $this->info('[CaseState] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('cases_states')->truncate();
-
-        $this->info('[CaseState] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('cases_states')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'cases_states', CasesState::class);
     }
 
     public function importDeathMalaysia()
     {
         $records = $this->covidService->getDeathMalaysia();
-
-        if (DB::table('deaths_malaysia')->count() == $records->count()) {
-            $this->info('[DeathMalaysia] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('deaths_malaysia')->truncate();
-
-        $this->info('[DeathMalaysia] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('deaths_malaysia')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'deaths_malaysia', DeathsMalaysia::class);
     }
 
     public function importDeathState()
     {
         $records = $this->covidService->getDeathState();
-
-        if (DB::table('deaths_states')->count() == $records->count()) {
-            $this->info('[DeathState] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('deaths_states')->truncate();
-
-        $this->info('[DeathState] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('deaths_states')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'deaths_states', DeathsState::class);
     }
 
     public function importTestMalaysia()
     {
         $records = $this->covidService->getTestMalaysia();
-
-        if (DB::table('test_malaysia')->count() == $records->count()) {
-            $this->info('[TestMalaysia] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('test_malaysia')->truncate();
-
-        $this->info('[TestMalaysia] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('test_malaysia')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'test_malaysia', TestMalaysia::class);
     }
 
     private function importTestState()
     {
         $records = $this->covidService->getTestState();
-
-        if (DB::table('test_states')->count() == $records->count()) {
-            $this->info('[TestState] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('test_states')->truncate();
-
-        $this->info('[TestState] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('test_states')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'test_states', TestState::class);
     }
 
     public function importCluster()
     {
         $records = $this->covidService->getCluster();
-
-        if (DB::table('clusters')->count() == $records->count()) {
-            $this->info('[Cluster] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-        DB::table('clusters')->truncate();
-
-        $this->info('[Cluster] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('clusters')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'clusters', Cluster::class);
     }
 
     public function importHospitals()
     {
         $records = $this->covidService->getHospitals();
-
-        if (DB::table('hospitals')->count() == $records->count()) {
-            $this->info('[Hospital] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('hospitals')->truncate();
-
-        $this->info('[Hospital] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('hospitals')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'hospitals', Hospital::class);
     }
 
     public function importICU()
     {
         $records = $this->covidService->getICU();
 
-        if (DB::table('icus')->count() == $records->count()) {
-            $this->info('[ICU] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('icus')->truncate();
-
-        $this->info('[ICU] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('icus')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'icus', ICU::class);
     }
 
     public function importPKRC()
     {
         $records = $this->covidService->getPKRC();
-
-        if (DB::table('PKRC')->count() == $records->count()) {
-            $this->info('[PKRC] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('PKRC')->truncate();
-
-        $this->info('[PKRC] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('PKRC')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'PKRC', PKRC::class);
     }
 
     public function importMalaysiaPopulation()
     {
         $records = $this->covidService->getMalaysiaPopulation();
+        $this->inject($records, 'populations', Population::class);
+    }
 
-        DB::table('populations')->truncate();
+    private function inject(Collection $records, string $tableName, string $modelName): void
+    {
+        if (DB::table($tableName)->count() == $records->count()) {
+            $this->info("[$modelName] : Not inject as the data is the same.");
+            return;
+        }
 
-        $this->info('[Population] : ' . 'Injecting...');
+        DB::table($tableName)->truncate();
+
+        $this->info("[$modelName] : Injecting...");
 
         $chunks = $records->chunk(500);
 
         $this->output->progressStart($chunks->count());
 
         foreach ($chunks as $chunk) {
-            DB::table('populations')->insert($chunk->toArray());
+            DB::table($tableName)->insert($chunk->toArray());
             $this->output->progressAdvance();
         }
 
--- app/Console/Commands/ImportVaxDataFromGithub.php
+++ app/Console/Commands/ImportVaxDataFromGithub.php
@@ -2,53 +2,33 @@
 
 namespace App\Console\Commands;
 
-use App\Console\Services\ImportCovidFromGithubService;
 use App\Console\Services\ImportVaccineFromGithubService;
 use App\Http\Services\WebHookService;
+use App\Models\Covid\VaxMalaysia;
+use App\Models\Covid\VaxRegMalaysia;
+use App\Models\Covid\VaxState;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 use Exception;
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Throwable;
 
 class ImportVaxDataFromGithub extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'import:vaccine {--force}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Import Vaccination Data From Github';
 
-    /**
-     * @var ImportCovidFromGithubService|Application|mixed
-     */
     private ImportVaccineFromGithubService $vaxService;
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
         $this->vaxService = app(ImportVaccineFromGithubService::class);
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
         try {
@@ -61,7 +41,7 @@ class ImportVaxDataFromGithub extends Command
             $this->importVaxRegState();
             $this->importVaxRegMalaysia();
 
-            cache()->clear();
+            Cache::clear();
             if (app()->environment('production')) {
                 app(WebHookService::class)->notifyInSpam(Carbon::now() . ' : Vaccine Data Successfully Inserted', WebHookService::COLOR['GREEN']);
             }
@@ -81,103 +61,48 @@ class ImportVaxDataFromGithub extends Command
         DB::table('vax_reg_malaysias')->truncate();
     }
 
-    /**
-     * @throws Throwable
-     */
     public function importVaxMalaysia()
     {
         $records = $this->vaxService->getVaxMalaysia();
-
-        if (DB::table('vax_malaysias')->count() == $records->count()) {
-            $this->info('[VaxMalaysia] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('vax_malaysias')->truncate();
-
-        $this->info('[VaxMalaysia] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('vax_malaysias')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'vax_malaysias', VaxMalaysia::class);
     }
 
     public function importVaxState()
     {
         $records = $this->vaxService->getVaxState();
-
-        if (DB::table('vax_states')->count() == $records->count()) {
-            $this->info('[VaxState] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('vax_states')->truncate();
-
-        $this->info('[VaxState] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('vax_states')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'vax_states', VaxState::class);
     }
 
     public function importVaxRegState()
     {
         $records = $this->vaxService->getVaxRegState();
-
-        if (DB::table('vax_reg_states')->count() == $records->count()) {
-            $this->info('[VaxRegState] : ' . 'Not inject as the data is the same.');
-            return;
-        }
-
-        DB::table('vax_reg_states')->truncate();
-
-        $this->info('[VaxRegState] : ' . 'Injecting...');
-
-        $chunks = $records->chunk(500);
-
-        $this->output->progressStart($chunks->count());
-
-        foreach ($chunks as $chunk) {
-            DB::table('vax_reg_states')->insert($chunk->toArray());
-            $this->output->progressAdvance();
-        }
-
-        $this->output->progressFinish();
+        $this->inject($records, 'vax_reg_states', VaxRegMalaysia::class);
     }
 
     public function importVaxRegMalaysia()
     {
         $records = $this->vaxService->getVaxRegMalaysia();
+        $this->inject($records, 'vax_reg_malaysias', VaxRegMalaysia::class);
 
-        if (DB::table('vax_reg_malaysias')->count() == $records->count()) {
-            $this->info('[VaxRegMalaysia] : ' . 'Not inject as the data is the same.');
+    }
+
+    private function inject(Collection $records, string $tableName, string $modelName): void
+    {
+        if (DB::table($tableName)->count() == $records->count()) {
+            $this->info("[$modelName] : Not inject as the data is the same.");
             return;
         }
 
-        DB::table('vax_reg_malaysias')->truncate();
+        DB::table($tableName)->truncate();
 
-        $this->info('[VaxRegMalaysia] : ' . 'Injecting...');
+        $this->info("[$modelName] : Injecting...");
 
         $chunks = $records->chunk(500);
 
         $this->output->progressStart($chunks->count());
 
         foreach ($chunks as $chunk) {
-            DB::table('vax_reg_malaysias')->insert($chunk->toArray());
+            DB::table($tableName)->insert($chunk->toArray());
             $this->output->progressAdvance();
         }
 
