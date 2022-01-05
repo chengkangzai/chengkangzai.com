4e2d2ab54f38a17b6d0bec14998f65b7fcbc29dd
chengkangzai <pycck@hotmail.com>
2022-01-05 17:47:40 +0800

    remove Clear cache upon modify dashboard Model, its okay the cache stay for 24 hours...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Post;
-use App\Models\Works;
-use Cache;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Spatie\Tags\Tag;
 use URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // for Maria DB
         Schema::defaultStringLength(191);
-        if($this->app->environment('production')) {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
 
@@ -39,29 +35,8 @@ class AppServiceProvider extends ServiceProvider
 
     }
 
-    public function registerClearCacheOnModel(){
-        Post::created(function () {
-            Cache::clear();
-        });
-
-        Post::updated(function () {
-            Cache::clear();
-        });
-
-        Works::created(function () {
-            Cache::clear();
-        });
-
-        Works::updated(function () {
-            Cache::clear();
-        });
-
-        Tag::created(function () {
-            Cache::clear();
-        });
-
-        Tag::updated(function () {
-            Cache::clear();
-        });
+    public function registerClearCacheOnModel()
+    {
+        //
     }
 }
