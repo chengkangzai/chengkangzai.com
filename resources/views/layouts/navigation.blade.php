b3ac8db94a1bced25e5e6f7b8cb30328910cd2ad
chengkangzai <pycck@hotmail.com>
2022-01-14 16:12:59 +0800

    migrate comment to new dashboard design

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -56,6 +56,17 @@
             {{ __('Works') }}
         </x-nav-link>
 
+        <x-nav-link href="{{ route('admin.comment.index') }}" :active="request()->is('admin/comment*')">
+            <x-slot name="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 inline" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
+                </svg>
+            </x-slot>
+            {{ __('Comments') }}
+        </x-nav-link>
+
         <x-divider class="mx-auto w-11/12"/>
 
         <x-nav-link href="{{ route('admin.users.index') }}" :active="request()->is('admin/users*')">
