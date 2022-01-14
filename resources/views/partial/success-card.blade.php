524d464b312f3c4ec86eb8e671adba8642bd2956
chengkangzai <pycck@hotmail.com>
2022-01-14 16:43:16 +0800

    change the color of the card

--- resources/views/partial/success-card.blade.php
+++ resources/views/partial/success-card.blade.php
@@ -1,7 +1,7 @@
 @if (session('success'))
     <div class="mb-2 w-4/5 mx-auto">
         <div
-            class="block text-sm text-left text-black font-bold bg-violet-200 border border-violet-400 h-auto flex items-center p-4 rounded-sm"
+            class="block text-sm text-left text-black font-bold bg-green-200 border border-green-400 h-auto flex items-center p-4 rounded-sm"
             role="alert">
             {!! session('success') !!}
         </div>
