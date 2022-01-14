6199c5536d0bfdde6ed49fb5815f41744ac6ea48
chengkangzai <pycck@hotmail.com>
2022-01-14 11:44:18 +0800

    abstract divider to component

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -49,9 +49,7 @@
             </form>
 
             @if (Route::has('login'))
-                <div class="relative flex justify-center items-center w-full border-b-2 border-gray-300 mt-4 ">
-                    <span class="absolute bg-gray-100 px-2"></span>
-                </div>
+                <x-divider/>
                 <div class="mt-4">
 
                     <a href="{{route('login')}}"
