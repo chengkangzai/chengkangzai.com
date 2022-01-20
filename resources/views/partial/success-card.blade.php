b398b58a69ca99da44a8be3b8ddfd0838706ed80
chengkangzai <pycck@hotmail.com>
2022-01-21 00:18:18 +0800

    update card UI

--- resources/views/partial/success-card.blade.php
+++ resources/views/partial/success-card.blade.php
@@ -1,9 +1,10 @@
 @if (session('success'))
-    <div class="mb-2 w-4/5 mx-auto">
-        <div
-            class="block text-sm text-left text-black font-bold bg-green-200 border border-green-400 h-auto flex items-center p-4 rounded-sm"
-            role="alert">
-            {!! session('success') !!}
+    <div
+        class="block text-sm text-left text-black bg-green-200 border border-green-400 h-auto flex items-center p-4 rounded-lg">
+        <div class="flex items-start">
+            <p class="ml-2">
+                {!! session('success') !!}
+            </p>
         </div>
     </div>
 @endif
