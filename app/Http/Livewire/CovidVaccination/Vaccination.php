2cfb8b6ac72011d0b5b37a51fd5dfe07d13d500d
chengkangzai <pycck@hotmail.com>
2022-03-26 17:31:18 +0800

    refactor
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- app/Http/Livewire/CovidVaccination/Vaccination.php
+++ app/Http/Livewire/CovidVaccination/Vaccination.php
@@ -30,16 +30,17 @@ class Vaccination extends Component
         $content = collect();
         $content->push("%License : Any form of copy or plagiarising of this file academically is strictly prohibited and consider as abuse.\n");
         $content->push("%Credit & Author : Ching Cheng Kang\n");
-        $content->push("%Downloaded Date : " . Carbon::now()->toDateTimeString() . "\n");
+        $content->push("%Downloaded Date : " . now()->toDateTimeString() . "\n");
         $content->push("%All right reserved\n\n");
-
         $content->push("%Vaccination Report\n");
         $content->push("%Facts \n");
 
-        $vaccinations = VaxRegState::latestOne()->get();
-        foreach ($vaccinations as $vaccination) {
-            $content->push("vaxreg(" . str($vaccination->state)->replace('W.P. ', '')->snake() . ",$vaccination->total).\n");
-        }
+        VaxRegState::latestOne()
+            ->get()
+            ->each(function ($vaccination) use ($content) {
+                $state = str($vaccination->state)->replace('W.P. ', '')->snake();
+                $content->push("vaxreg($state,$vaccination->total).\n");
+            });
 
         $content->push("\n");
         $content->push("printAll:-\n");
