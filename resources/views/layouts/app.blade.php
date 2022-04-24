8af9fcabbb28437494b840555d4b79f55466fd0f
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-24 22:20:30 +0800

    only show Blog when the publish post is more than 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
                        href="#work">{{__('My Work')}}</a>
                     <a class="px-2 py-2 mt-2 text-md font-bold text-white bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                        href="#experience">{{__('Experience')}}</a>
-                    @if(\App\Models\Post::count() > 0)
+                    @if(\App\Models\Post::published()->count() > 0)
                         <a class="px-2 py-2 mt-2 text-md font-bold text-white bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                            href="{{route('public.posts.index')}}">{{__('Blog')}}</a>
                     @endif
