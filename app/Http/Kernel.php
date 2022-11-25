040adacbff993a8e6b24e2b82f5adb933ca39fdf
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-11-25 10:56:53 +0800

    introduce bugsnag

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,4 +69,12 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
+
+    protected function bootstrappers(): array
+    {
+        return array_merge(
+            [\Bugsnag\BugsnagLaravel\OomBootstrapper::class],
+            parent::bootstrappers(),
+        );
+    }
 }
