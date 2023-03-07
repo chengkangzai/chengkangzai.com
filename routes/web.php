0ff14c3175e258c49c9eb83731c05ab360c78334
Ching Cheng Kang <pycck@hotmail.com>
2023-03-07 21:03:45 +0800

    improve sitemap

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['as' => 'public.'], function () {
     Route::get('/', [PublicIndexController::class, 'index'])->name('index');
     Route::get('/locale/{locale}', [LocaleController::class, 'changeLocale'])->name('setLocale');
-    Route::redirect('resume', 'resume.pdf');
+    Route::redirect('resume', 'resume.pdf')->name('resume');
 
     Route::get('unsubscribe/{email}', function ($email) {
         User::where('email', $email)->firstOrFail()->scheduleConfig()->firstOrFail()->updateOrFail(['is_subscribed' => false]);
