f47bc999003345e12db3390a3dbe17923952f1eb
Ching Cheng Kang <pycck@hotmail.com>
2023-05-19 12:53:17 +0800

    hotfix

--- app/Services/GetTopGithubCommitRankService.php
+++ app/Services/GetTopGithubCommitRankService.php
@@ -2,23 +2,28 @@
 
 namespace App\Services;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class GetTopGithubCommitRankService
 {
     public function getTopGithubCommitRank(): int
     {
-        $response = Http::get('https://commits.top/malaysia.html');
-        $body = $response->body();
-        $rows = str($body)
-            ->between('<tbody>', '</tbody>')
-            ->explode('<tr>');
+        try {
+            $response = Http::get('https://commits.top/malaysia.html');
+            $body = $response->body();
+            $rows = str($body)
+                ->between('<tbody>', '</tbody>')
+                ->explode('<tr>');
 
-        return collect($rows)
-            ->filter(fn ($item) => str($item)->contains('chengkangzai'))
-            ->map(fn ($_, $index) => $index)
-            ->whenEmpty(fn () => collect([0 => 0]))
-            ->values()
-            ->first();
+            return collect($rows)
+                ->filter(fn($item) => str($item)->contains('chengkangzai'))
+                ->map(fn($_, $index) => $index)
+                ->whenEmpty(fn() => collect([0 => 0]))
+                ->values()
+                ->first();
+        } catch (Exception $exception) {
+            return 0;
+        }
     }
 }
