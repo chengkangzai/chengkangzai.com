1b8c4e40243e73ee1d5b6c1bcf5dbfdf7a826506
Ching Cheng Kang <pycck@hotmail.com>
2024-04-11 21:11:50 +0800

    update email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@admin.com',
         ]);
     }
 }
