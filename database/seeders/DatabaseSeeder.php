b2294b3bfe8cf2dc577d3f3d36061acc4cb29238
Ching Cheng Kang <pycck@hotmail.com>
2024-05-04 00:33:12 +0800

    add resume

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,12 +5,15 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         // User::factory(10)->create();
+
+        Storage::disk('public')->put('resume.pdf', Storage::disk('local-seed')->get('resume.pdf'));
 
         User::factory()->create([
             'name' => 'Test User',
