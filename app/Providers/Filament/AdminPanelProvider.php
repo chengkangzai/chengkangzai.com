e33fcd949cafb606abbd6eb8744012896778f2db
Ching Cheng Kang <pycck@hotmail.com>
2026-03-11 11:20:54 +0800

    add profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
             ->colors([
                 'primary' => Color::Amber,
             ])
